e5073de60764021ce3ef07ed1c7e3c192932b95c
Ashok Yadav <ashokyadav006@gmail.com>
2016-04-11 11:54:40 +0530

    Added webpack

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp'),
-	  babel = require('gulp-babel');
-
-gulp.task('default', function() {
-	return gulp.src("src/*.js")
-		.pipe(babel())
-		.pipe(gulp(dest("dist")))
-});
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var webpack = require('webpack');
+var path = require('path');
+var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+var env = process.env.WEBPACK_ENV;
+
+var libraryName = 'annotatePDF';
+var outputFile, plugins = [];
+
+if(env === 'build') {
+	plugins.push(new UglifyJsPlugin({ minimize: true }));
+  	outputFile = libraryName + '.min.js';
+} else {
+	outputFile = libraryName + '.js';
+}
+
+var config = {
+	entry: __dirname + '/src/annotePdf.js',
+	devtool: 'source-map',
+	output: {
+	    path: __dirname + '/dist',
+	    filename: outputFile,
+	    library: libraryName,
+	    libraryTarget: 'umd',
+	    umdNamedDefine: true
+	},
+	module: {
+	    loaders: [
+	      {
+	        test: /(\.jsx|\.js)$/,
+	        loader: 'babel',
+	        exclude: /(node_modules|bower_components)/
+	      },
+	      {
+	        test: /(\.jsx|\.js)$/,
+	        loader: "eslint-loader",
+	        exclude: /node_modules/
+	      }
+	    ]
+	},
+	resolve: {
+	    root: path.resolve('./src'),
+	    extensions: ['', '.js']
+	},
+	plugins: plugins
+};
+
+module.exports = config;
